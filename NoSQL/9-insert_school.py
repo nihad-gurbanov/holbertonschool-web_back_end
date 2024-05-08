8125cefe4e0eb5131bc9811f01cd4146b294cbe7
gurbanovnihad <7404@holbertonstudents.com>
2024-05-08 23:46:45 +0400

    insert a doc Python

---
+++ NoSQL/9-insert_school.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+"""
+    It is a Python function that inserts a new document in a collection
+    based on kwargs.
+"""
+
+import pymongo
+
+
+def insert_school(mongo_collection, **kwargs):
+
+    """
+        Insert a new document into a MongoDB collection based
+        on keyword arguments.
+
+        Args:
+            - mongo_collection: A pymongo collection object.
+            - **kwargs: Keyword arguments representing the fields and values
+            of the document to be inserted.
+
+        Returns:
+            - The _id of the newly inserted document.
+    """
+    mongo_collection.insert_one(kwargs)
