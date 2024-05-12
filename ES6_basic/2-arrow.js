3e7fd151bf30be3c9994cb8d1e5e7003f6384fbd
gurbanovnihad <7404@holbertonstudents.com>
2024-05-12 15:17:56 +0400

    arrow functions

---
+++ ES6_basic/2-arrow.js
@@ -0,0 +1,10 @@
+/* eslint-disable linebreak-style */
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newNeighborhood) => {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
