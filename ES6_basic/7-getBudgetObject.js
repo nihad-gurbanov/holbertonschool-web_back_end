ed76d7b686bebabc78eb50e5068a9eaef6376793
gurbanovnihad <7404@holbertonstudents.com>
2024-05-12 15:54:34 +0400

    object property value shorthand syntax

---
+++ ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,10 @@
+/* eslint-disable linebreak-style */
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
