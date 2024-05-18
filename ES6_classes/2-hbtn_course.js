37b7b82efccbd0c62db9878a8c292d9e71d84ebe
gurbanovnihad <7404@holbertonstudents.com>
2024-05-18 22:18:57 +0400

    ES6 class getter setters

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,4 +1,4 @@
-export class HolbertonCourse {
+export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
