b2059b0905da2dfa8045377416343b29c272dc39
gurbanovnihad <7404@holbertonstudents.com>
2024-05-24 15:01:36 +0400

    javascript promises .then .catch

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,11 +1,9 @@
 function handleResponseFromAPI(promise) {
   return promise
-    .then(()=>{
+    .then(() => {
       console.log('Got a response from the API');
       return { status: 200, body: 'success' };
-    }).catch(() => {
-      return new Error();
-    });
+    }).catch(() => new Error());
 }
 
 export default handleResponseFromAPI;
