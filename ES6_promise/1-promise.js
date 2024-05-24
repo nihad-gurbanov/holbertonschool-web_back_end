20e641919222f2dc884d6e0d639fa98881e52f93
gurbanovnihad <7404@holbertonstudents.com>
2024-05-24 14:34:39 +0400

    javascript promises with boolean parameter

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,9 +1,9 @@
-function getFullResponseFromAPI (success) {
+function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({ status: 200, body: 'Success' });
     } else {
-      reject(new Error('fake API is not working currently'));
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
