f85d1dfe15f26ef1197273adb630f48db4049eaa
gurbanovnihad <7404@holbertonstudents.com>
2024-05-25 13:13:41 +0400

    handling  multiple promises

--- ES6_promise/4-main.js
+++ ES6_promise/4-main.js
@@ -1,4 +1,4 @@
 // 4-main.js
-import signUpUser from "./4-user-promise";
+import signUpUser from './4-user-promise';
 
-console.log(signUpUser("Bob", "Dylan"));
+console.log(signUpUser('Bob', 'Dylan'));
