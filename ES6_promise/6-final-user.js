41ae5e9663148234bf82ac760e68b0f4c2360ae3
gurbanovnihad <7404@holbertonstudents.com>
2024-05-25 13:06:20 +0400

    handling  multiple promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,21 +1,13 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
 
-async function handleProfileSignup(firstName, lastName, fileName) {
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPromise = uploadPhoto(fileName);
-
-  try {
-    const [signUpResult, uploadResult] = await Promise.allSettled([signUpPromise, uploadPromise]);
-
-    return [
-      { status: signUpResult.status, value: signUpResult.status === 'fulfilled' ? signUpResult.value : signUpResult.reason },
-      { status: uploadResult.status, value: uploadResult.status === 'fulfilled' ? uploadResult.value : uploadResult.reason }
-    ];
-  } catch (error) {
-    console.error('Error during signup process:', error);
-    return [];
-  }
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promises = [signUpUser(firstName, lastName), uploadPhoto(fileName)];
+  return Promise.allSettled(promises).then((results) =>
+    results.map((result) => ({
+      status: result.status,
+      value:
+        result.status === "fulfilled" ? result.value : String(result.reason),
+    }))
+  );
 }
-
-export default handleProfileSignup;
