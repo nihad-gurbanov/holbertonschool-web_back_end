33161f93a314514390a10feb110c32bbc21f4304
gurbanovnihad <7404@holbertonstudents.com>
2024-05-26 12:05:53 +0400

    filter map join array

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,8 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') {
+  if (typeof startString !== 'string' || startString === '') {
     return '';
   }
-  
+
   return [...set]
     .filter((value) => value.startsWith(startString))
     .map((value) => value.substring(startString.length))
